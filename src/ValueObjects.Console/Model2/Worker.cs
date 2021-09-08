cfbd0adc86834d45944040869eeed57c6e186f57
Steve Smith <steve@kentsmiths.com>
2021-09-07 20:14:30 -0400

    Reformmating and adding private setters

--- src/ValueObjects.Console/Model2/Worker.cs
+++ src/ValueObjects.Console/Model2/Worker.cs
@@ -2,6 +2,10 @@
 
 public class Worker : BaseEntity
 {
+    public Name Name { get; set; }
+    public DateTimeRange EmploymentDates { get; set; }
+    public Address Address { get; set; }
+
     public Worker(Name name, DateTimeRange employmentDates, Address address)
     {
         Name = name;
@@ -9,15 +13,15 @@ public class Worker : BaseEntity
         Address = address;
     }
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     private Worker() { } // EF
-
-    public Name Name { get; set; }
-    public DateTimeRange EmploymentDates { get; set; }
-    public Address Address { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
     public override string ToString()
     {
         return $"{Name}\n{EmploymentDates}\n{Address}";
     }
+
+
 }
 
